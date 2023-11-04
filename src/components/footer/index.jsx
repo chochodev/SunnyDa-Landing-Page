87fc06363096203fb675d82e3b180e4970414334
chochoprogrammer <michochoprogrammer@gmail.com>
2023-11-04 16:12:48 +0100

    updated with the right copyright text from the design

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -34,7 +34,7 @@ const Footer = () => {
         </div>
       </div>
     </div>
-    <small className="copyright">Copyright &copy; {year} SunnyDay Trailers. All right reserved.</small>
+    <small className="copyright">Copyright &copy; {year} SunnyDay.com All Right Reserved.</small>
     </>
   )
 }
