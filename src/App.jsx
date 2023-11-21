0c3d87b5aa8ea458ecd397faeaa51b5503886dbc
chochoprogrammer <michochoprogrammer@gmail.com>
2023-11-21 03:30:07 +0100

    updated with start the about and contact us pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import Home from './pages/home';
+import About from './pages/about';
+import Contact from './pages/contact';
 
 function App() {
 
@@ -8,6 +10,8 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
       </Routes>
     </Router>
   )
