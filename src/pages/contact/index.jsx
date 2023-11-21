a1ccfe84ca67f3e8d05ec7fae192a7826d6070af
chochoprogrammer <michochoprogrammer@gmail.com>
2023-11-21 03:25:18 +0100

    updated with start the about and contact us pages

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -13,7 +13,16 @@ const Contact = () => {
             </div>
 
             <div id="section_2" className="section_2">
-                
+                <div className="text_div">
+                    <p className="head">About DesignHub</p>
+                    <p className="heading">Introduction To The Best<br/> <p>Digital Agency</p></p>
+                    <p className="info">Sunnyda Movie Trailers is the sole manufacturer of these purpose-built trailers for the Australian movie industry. As a team of Queensland-based film industry professionals, we understand the market intimately and are poised for significant growth and profitability.<br/><br/>Are you ready to be a part of the thrilling world of movies and make a smart investment at the same time? Sunnyda Movie Trailers invites you to join us on an exciting journey into the booming movie industry in Australia.</p>
+                    <button className="invest_btn">Invest Now</button>
+                </div>
+                <div className="images">
+                    <img src="image5.jpeg" alt="sunnyda" className='img' />
+                    <img src="image4.jpeg" alt="sunnyda" className='img _2' />
+                </div>
             </div>
         </div>
     </MainLayout>
