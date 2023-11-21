07a02f324c3ffcdf71e3c9b6d3a9641ad20ea7dd
chochoprogrammer <michochoprogrammer@gmail.com>
2023-11-21 03:28:08 +0100

    updated with start the about and contact us pages

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
 import MainLayout from '../../components/layout';
+import '../../assets/css/pages/about.css';
 
 const About = () => {
   return (
-    <MainLayout>About</MainLayout>
+    <MainLayout>
+        <div id="contact">
+            <div id="hero" className="hero">
+                <h3 className="text">Welcome To SunnyDa</h3>
+                <h2 className="heading">Your Gateway to Lucrative Cinematic Investments!</h2>
+                <p className="info">Are you ready to be a part of the thrilling world of movies and make a smart investment at the same time? Sunnyda Movie Trailers invites you to join us on an exciting journey into the booming movie industry in Australia.</p>
+                <button className="invest_btn">Invest with Us</button>
+            </div>
+
+            <div id="section_2" className="section_2">
+                <div className="text_div">
+                    <p className="head">About DesignHub</p>
+                    <p className="heading">Introduction To The Best<br/> <p>Digital Agency</p></p>
+                    <p className="info">Sunnyda Movie Trailers is the sole manufacturer of these purpose-built trailers for the Australian movie industry. As a team of Queensland-based film industry professionals, we understand the market intimately and are poised for significant growth and profitability.<br/><br/>Are you ready to be a part of the thrilling world of movies and make a smart investment at the same time? Sunnyda Movie Trailers invites you to join us on an exciting journey into the booming movie industry in Australia.</p>
+                    <button className="invest_btn">Invest Now</button>
+                </div>
+                <div className="images">
+                    <img src="image5.jpeg" alt="sunnyda" className='img' />
+                    <img src="image4.jpeg" alt="sunnyda" className='img _2' />
+                </div>
+            </div>
+        </div>
+    </MainLayout>
   )
 }
 
