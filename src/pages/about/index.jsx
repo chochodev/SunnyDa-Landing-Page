b7622085d975dd6e8063f5a96eef44f53975b0f1
chochoprogrammer <michochoprogrammer@gmail.com>
2023-11-21 03:39:44 +0100

    updated with start the about and contact us pages

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -5,10 +5,10 @@ import '../../assets/css/pages/about.css';
 const About = () => {
   return (
     <MainLayout>
-        <div id="contact">
+        <div id="about">
             <div id="hero" className="hero">
-                <h3 className="text">Welcome To SunnyDa</h3>
-                <h2 className="heading">Your Gateway to Lucrative Cinematic Investments!</h2>
+                <h3 className="text">Welcome To <p>SunnyDa</p></h3>
+                <h2 className="heading">Your Gateway to Lucrative<br/>Cinematic Investments!</h2>
                 <p className="info">Are you ready to be a part of the thrilling world of movies and make a smart investment at the same time? Sunnyda Movie Trailers invites you to join us on an exciting journey into the booming movie industry in Australia.</p>
                 <button className="invest_btn">Invest with Us</button>
             </div>
