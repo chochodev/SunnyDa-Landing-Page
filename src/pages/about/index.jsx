34d7613be7644f793d9cd7fa63344fca74af8888
chochoprogrammer <michochoprogrammer@gmail.com>
2023-11-22 10:53:37 +0100

    updated and about to finish up

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -14,7 +14,7 @@ const About = () => {
 
             <div id="section_2" className="section_2 section">
                 <div className="text_div">
-                    <h3 className="text">About <p>SunnyDa</p></h3>
+                    <h3 className="text">About <h3>SunnyDa</h3></h3>
                     <p className="info">At Sunnyda Movie Trailers, we are driven by a shared passion for the captivating world of cinema and a commitment to intelligent investment. As enthusiasts of the booming movie industry in Australia, we invite you to explore the heart of our vision. Founded on the belief that the magic of movies can be both a thrilling experience and a smart investment opportunity, Sunnyda Movie Trailers welcomes you to join us on this exciting journey. 
                     <br/><br/>
                     Our mission is to build a vibrant community of individuals who share our enthusiasm for the Australian film industry, fostering a space where creativity and financial wisdom converge seamlessly. Discover the story behind our commitment to redefining the narrative, where the excitement of movies intersects with shrewd financial choices, shaping a future that resonates with the magic and potential of the silver screen.</p>
@@ -28,7 +28,7 @@ const About = () => {
 
             <div id="section_3" className="section_3 section">
                 <div className="text_div">
-                    <p className="text">Vision </p>
+                    <h3 className="text">Vision </h3>
                     <p className="info">Empower your cinematic aspirations and financial future with Sunnyda Movie Trailers.  Our vision is to foster a community that passionately embraces the burgeoning opportunities within the Australian film industry, creating a synergistic blend of creativity and financial acumen. Join us as we redefine the narrative, bridging the gap between cinematic excitement and astute investment, shaping a future where the magic of movies intertwines seamlessly with smart financial choices.</p>
                     <button className="invest_btn">Invest Now</button>
                 </div>
